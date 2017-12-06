3bb7ad1d89acf6ff98fa817fc69cc5df28dd742a
Max Bo <max@maxbo.me>
2017-12-06 10:13:51 +1000

    Rename

--- Day_3.py
+++ Day_3.py
@@ -12,7 +12,7 @@ square, pos, found = 2, (1 + 0j), False
 grid = defaultdict(int)
 grid[(0 + 0j)], grid[(1 + 0j)] = 1, 1
 
-def log(): 
+def check_square(): 
     if square == INPUT: print(f"Part 1: {int(distance(pos))}"); exit(0) # 480
 
 def neighbours_sum(): return sum(grid[pos + i] for i in [N, S, E, W, NE, SE, SW, NW])
@@ -23,7 +23,7 @@ def spread():
     if grid[pos] > INPUT and not found: print(f"Part 2: {grid[pos]}"); found = True # 349975
 
 for i in range(2, BIG, 2): # start at side length 3
-    for _ in range(i - 1): pos += N; square += 1; spread(); log()
-    for _ in range(i):     pos += W; square += 1; spread(); log()
-    for _ in range(i):     pos += S; square += 1; spread(); log()
-    for _ in range(i + 1): pos += E; square += 1; spread(); log()
+    for _ in range(i - 1): pos += N; square += 1; spread(); check_square()
+    for _ in range(i):     pos += W; square += 1; spread(); check_square()
+    for _ in range(i):     pos += S; square += 1; spread(); check_square()
+    for _ in range(i + 1): pos += E; square += 1; spread(); check_square()

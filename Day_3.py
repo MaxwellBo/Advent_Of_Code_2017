8a2ae4ccaca0ea5252268cf203fddc88ee27d272
Max Bo <max@maxbo.me>
2017-12-06 10:00:47 +1000

    Commit great sins completing Day 3

--- Day_3.py
+++ Day_3.py
@@ -1,23 +1,29 @@
 from Day_0 import *
+from collections import defaultdict
 
 INPUT = 347991
-
-Point = complex
 N, S, E, W = 1j, -1j, 1, -1
+NE, SE, SW, NW = (N + E), (S + E), (S + W), (N + W)
 
 def distance(point): return abs(point.real) + abs(point.imag)
 
-square = 2
-pos = 1
+square, pos, found = 2, 1, False
 
-def log():
-    if (square == INPUT): print(f"Day 1: {distance(pos)}"); return True else: return False
+grid = defaultdict(int)
+grid[0], grid[1] = 1, 1
+
+def log(): 
+    if square == INPUT: print(f"Day 1: {int(distance(pos))}"); exit(0)
+
+def neighbours_sum(): return sum(grid[pos + i] for i in [N, S, E, W, NE, SE, SW, NW])
+
+def spread():
+    global found
+    grid[pos] = neighbours_sum()
+    if grid[pos] > INPUT and not found: print(f"Part 2: {grid[pos]}"); found = True
 
 for i in range(2, BIG, 2): # start at side length 3
-    for _ in range(i - 1): pos += N; square += 1; log()
-    for _ in range(i): pos += W; square += 1; log()
-    for _ in range(i): pos += S; square += 1; log()
-    for _ in range(i + 1): pos += E; square += 1; log()
-
-    print(square, pos)
-
+    for _ in range(i - 1): pos += N; square += 1; spread(); log()
+    for _ in range(i):     pos += W; square += 1; spread(); log()
+    for _ in range(i):     pos += S; square += 1; spread(); log()
+    for _ in range(i + 1): pos += E; square += 1; spread(); log()

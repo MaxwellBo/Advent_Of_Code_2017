240c1685531fae29b8bcc8a27014bfc0114547d2
Max Bo <max@maxbo.me>
2017-12-07 13:22:43 +1000

    Complete Day 6 significantly overtime

--- Day_0.py
+++ Day_0.py
@@ -1,5 +1,5 @@
-def getDayInput(number):
-    file = open('inputs.txt', 'r')
+def get_day_input(number: int):
+    file = open(f'inputs/day_{number}.txt', 'r')
     text = file.read()
     file.close()
     return text
---
+++ Day_6.py
@@ -0,0 +1,22 @@
+from Day_0 import get_day_input
+
+blocks, history = [ int(i) for i in get_day_input(6).split('\t') ], []
+
+while True:
+    max_index = blocks.index(max(blocks))
+    pointer = max_index
+    
+    max_val, blocks[max_index] = blocks[max_index], 0
+
+    while max_val > 0:
+        pointer = 0 if (pointer + 1) == len(blocks) else pointer + 1
+        max_val -= 1
+        blocks[pointer] += 1
+
+    if tuple(blocks) in history: break
+
+    history.append(tuple(blocks))
+
+print(f"Part 1: {len(history) + 1}") # 11137
+print(f"Part 2: {len(history) - history.index(tuple(blocks))}") # 1037
+

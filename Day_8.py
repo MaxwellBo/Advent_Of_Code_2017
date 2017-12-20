1ab8d301913fc5880bb9f96e30135e80a4ab8db1
Max Bo <max@maxbo.me>
2017-12-20 15:50:40 +1000

    Some stuff

---
+++ Day_8.py
@@ -0,0 +1,12 @@
+from Day_0 import get_day_input
+from collections import defaultdict
+
+d, history = defaultdict(lambda: 0), []
+instructions = [ i.split(' ') for i in get_day_input(8).replace("inc", "+=").replace("dec", "-=").split('\n')[:-1] ]
+
+for xp in instructions:
+    exec(" ".join(["if", f"d['{xp[4]}']", xp[5], xp[6], ':', f"d['{xp[0]}']", xp[1], xp[2]]))
+    history.append(max(d.values()))
+
+print(f"Part 1: {max(d.values())}")
+print(f"Part 2: {max(history)}")

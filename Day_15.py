56fe7bcc48dab853fc3f9bd489e0bcf7cea499b4
Max Bo <max@maxbo.me>
2017-12-21 13:53:31 +1000

    Complete Day 15

--- Day_15.py
+++ Day_15.py
@@ -16,14 +16,14 @@ def gen(start, factor, multiple=None):
 a, am = gen(A_START, A_FACTOR), gen(A_START, A_FACTOR, A_MULTIPLE)
 b, bm = gen(B_START, B_FACTOR), gen(B_START, B_FACTOR, B_MULTIPLE)
 
-for i in range(40000000):
+for _ in range(40000000):
     if next(a) & 65535 == next(b) & 65535: count += 1
 
 print("Part 15-1: {count}".format(count=count)) # 600
 
 count = 0 
 
-for i in range(5000000):
+for _ in range(5000000):
     if next(am) & 65535 == next(bm) & 65535: count += 1
 
-print("Part 12-2: {count}".format(count=count)) # 313
+print("Part 15-2: {count}".format(count=count)) # 313

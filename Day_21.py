1e9d293396615083d16ed8933930dbba7f7ef0ae
Max Bo <max@maxbo.me>
2017-12-22 16:42:41 +1000

    Update

--- Day_21.py
+++ Day_21.py
@@ -1,14 +1,10 @@
 import numpy as np
 from Day_0 import get_day_input
 
-mutations = [ lambda x: x
-            , lambda x: np.rot90(x, 1) 
-            , lambda x: np.rot90(x, 2) 
-            , lambda x: np.rot90(x, 3) 
-            , lambda x: np.fliplr(x)
-            , lambda x: np.fliplr(np.rot90(x, 1))
-            , lambda x: np.fliplr(np.rot90(x, 2))
-            , lambda x: np.fliplr(np.rot90(x, 3))
+mutations = [ lambda x: x,              lambda x: np.fliplr(x)
+            , lambda x: np.rot90(x, 1), lambda x: np.fliplr(np.rot90(x, 1))
+            , lambda x: np.rot90(x, 2), lambda x: np.fliplr(np.rot90(x, 2))
+            , lambda x: np.rot90(x, 3), lambda x: np.fliplr(np.rot90(x, 3))
             ]
 
 def parse(xs):
@@ -24,14 +20,14 @@ enhancements = { parse(i.split()[0]).tostring() : parse(i.split()[2])\
 
 arr = parse(".#./..#/###")
 
-for i in range(18):
+for i in range(1, 19):
     axis = arr.shape[0]
 
     divided = [ np.hsplit(i, axis / 2) for i in np.vsplit(arr, axis / 2) ]\
-    if axis % 2 == 0 else\
-    [ np.hsplit(i, axis / 3) for i in np.vsplit(arr, axis / 3) ]
+        if axis % 2 == 0 else\
+              [ np.hsplit(i, axis / 3) for i in np.vsplit(arr, axis / 3) ]
 
-    arr = np.vstack([ np.hstack( [ enhance(j) for j in i ] ) for i in divided ])
+    arr = np.vstack([ np.hstack([ enhance(j) for j in i ]) for i in divided ])
 
-    if i == 4: print(f"Day 21-1: {np.count_nonzero(arr == '#')}") # 142
-    if i == 17: print(f"Day 21-2: {np.count_nonzero(arr == '#')}") # 1879071
+    if i == 5: print(f"Day 21-1: {np.count_nonzero(arr == '#')}") # 142
+    if i == 18: print(f"Day 21-2: {np.count_nonzero(arr == '#')}") # 1879071

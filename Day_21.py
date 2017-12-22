b69e3ccc0851993d8540abf55f694aa043880c4f
Max Bo <max@maxbo.me>
2017-12-22 16:26:54 +1000

    Update Day 21

--- Day_21.py
+++ Day_21.py
@@ -12,34 +12,26 @@ mutations = [ lambda x: x
             ]
 
 def parse(xs):
-    return np.array([[ col for col in row ] for row in xs.split('/') ])
+    return np.array([list(row) for row in xs.split('/') ])
 
 def enhance(a):
     for f in mutations:
         if f(a).tostring() in enhancements: 
             return enhancements[f(a).tostring()]
 
-
-enhancements = {}
-
-for enhancement in get_day_input(21).strip().split('\n'):
-    frm, _, to = enhancement.split()
-    enhancements[parse(frm).tostring()] = parse(to)
+enhancements = { parse(i.split()[0]).tostring() : parse(i.split()[2])\
+                for i in get_day_input(21).strip().split('\n') }
 
 arr = parse(".#./..#/###")
 
 for i in range(18):
-    divided = []
     axis = arr.shape[0]
 
-    if axis % 2 == 0:
-        divided = [ np.hsplit(i, axis / 2) for i in np.vsplit(arr, axis / 2) ]
-    else:
-        divided = [ np.hsplit(i, axis / 3) for i in np.vsplit(arr, axis / 3) ]
+    divided = [ np.hsplit(i, axis / 2) for i in np.vsplit(arr, axis / 2) ]\
+    if axis % 2 == 0 else\
+    [ np.hsplit(i, axis / 3) for i in np.vsplit(arr, axis / 3) ]
 
     arr = np.vstack([ np.hstack( [ enhance(j) for j in i ] ) for i in divided ])
 
     if i == 4: print(f"Day 21-1: {np.count_nonzero(arr == '#')}") # 142
     if i == 17: print(f"Day 21-2: {np.count_nonzero(arr == '#')}") # 1879071
-
-

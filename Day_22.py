5796a034229a4cd00324abdcc5e402496654208b
Max Bo <max@maxbo.me>
2017-12-25 19:01:24 +1000

    Remember how complex numbers work

--- Day_22.py
+++ Day_22.py
@@ -2,7 +2,7 @@
 from Day_0 import *
 from collections import defaultdict
 
-CLEAN, WEAKENED, INFECTED, FLAGGED, DIRECTIONS = '.', 'W', '#', 'F', [N, E, S, W]
+CLEAN, WEAKENED, INFECTED, FLAGGED = '.', 'W', '#', 'F'
 
 def load_grid():
     grid = defaultdict(lambda: '.')
@@ -13,41 +13,36 @@ def load_grid():
 
     return grid 
 
-def part_1():
-    facing, pos, infected, grid = 0, (0 + 0j), 0, load_grid()
+facing, pos, infected, grid = 1j, (0 + 0j), 0, load_grid()
 
-    for i in range(10000):
-        if grid[pos] == CLEAN:
-            facing -= 1 # turn left
-            grid[pos] = INFECTED; infected += 1
-        elif grid[pos] == INFECTED:
-            facing += 1 # turn right
-            grid[pos] = CLEAN
+for i in range(10000):
+    if grid[pos] == CLEAN:
+        facing *= 1j # turn left
+        grid[pos] = INFECTED; infected += 1
+    elif grid[pos] == INFECTED:
+        facing *= -1j # turn right
+        grid[pos] = CLEAN
 
-        pos += DIRECTIONS[facing % len(DIRECTIONS)]
+    pos += facing
 
-    print(f'Day 22-1: {infected}') # 5399
+print(f'Day 22-1: {infected}') # 5399
 
-def part_2():
-    facing, pos, infected, grid = 0, (0 + 0j), 0, load_grid()
+facing, pos, infected, grid = 1j, (0 + 0j), 0, load_grid()
 
-    for i in range(10000000):
-        if grid[pos] == CLEAN:
-            facing -= 1 # turn left
-            grid[pos] = WEAKENED
-        elif grid[pos] == WEAKENED:
-            # continue straight
-            grid[pos] = INFECTED; infected += 1
-        elif grid[pos] == INFECTED:
-            facing += 1 # turn right
-            grid[pos] = FLAGGED
-        elif grid[pos] == FLAGGED:
-            facing += len(DIRECTIONS) // 2 # about-face
-            grid[pos] = CLEAN
+for i in range(10000000):
+    if grid[pos] == CLEAN:
+        facing *= 1j # turn left
+        grid[pos] = WEAKENED
+    elif grid[pos] == WEAKENED:
+        # continue straight
+        grid[pos] = INFECTED; infected += 1
+    elif grid[pos] == INFECTED:
+        facing *= -1j # turn right
+        grid[pos] = FLAGGED
+    elif grid[pos] == FLAGGED:
+        facing *= -1 # about face
+        grid[pos] = CLEAN
 
-        pos += DIRECTIONS[facing % len(DIRECTIONS)]
+    pos += facing
 
-    print(f'Day 22-2: {infected}') # 2511776
-
-part_1()
-part_2()
+print(f'Day 22-2: {infected}') # 2511776
